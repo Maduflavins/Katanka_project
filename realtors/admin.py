300b1adab3e6566ecb5f4714120c76d0537a26a5
maduabuchi <maduabuchiokonkwo@gmail.com>
2019-01-16 17:39:13 +0100

    pull data from listings and add pagination

--- realtors/admin.py
+++ realtors/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Realtor
 
 # Register your models here.
+
+class RealtorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email', 'hire_date')
+    list_dispaly_links = ('id', 'name')
+    lidt_per_page = 25
+
+
+admin.site.register(Realtor, RealtorAdmin)
